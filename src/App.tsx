dc85621d6e80bae48a0f7e6a0cb3f375208677c8
NFA23SCM32B <njosephbinu@hawk.iit.edu>
2025-01-30 23:41:05 -0600

    init3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Search from "./pages/search";
 
